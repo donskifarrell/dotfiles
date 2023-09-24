b323ecbda2d8a3d1f0dd3e647a7dab1480ca9bbf
Donal Farrell <donskifarrell@googlemail.com>
2023-09-24 21:57:37 +0800

    cleanup for hyprland config

--- makati-nixos/packages.nix
+++ makati-nixos/packages.nix
@@ -67,6 +67,7 @@ in
     rofi-wayland
     nwg-dock-hyprland
     swaylock
+    swaynotificationcenter
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
