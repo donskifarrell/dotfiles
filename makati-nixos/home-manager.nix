95e4675a76c4632d08a503d12afd863efc89aa97
Donal Farrell <donskifarrell@googlemail.com>
2023-09-25 13:00:56 +0800

    adding config files

--- makati-nixos/home-manager.nix
+++ makati-nixos/home-manager.nix
@@ -64,6 +64,55 @@ in {
       waybar = {
         enable = true;
         systemd.enable = true;
+        settings = {
+          mainBar = {
+            layer = "top";
+            position = "top";
+            height = 30;
+            # output = [
+            #   "eDP-1"
+            #   "HDMI-A-1"
+            # ];
+            # modules-left = ["sway/workspaces" "sway/mode" "wlr/taskbar"];
+            # modules-center = ["sway/window" "custom/hello-from-waybar"];
+            # modules-right = ["mpd" "custom/mymodule#with-css-id" "temperature"];
+
+            # "sway/workspaces" = {
+            #   disable-scroll = true;
+            #   all-outputs = true;
+            # };
+            "custom/hello-from-waybar" = {
+              format = "hello {}";
+              max-length = 40;
+              interval = "once";
+              exec = pkgs.writeShellScript "hello-from-waybar" ''
+                echo "from within waybar"
+              '';
+            };
+
+            "custom/notification" = {
+              tooltip = false;
+              format = "{icon}";
+              format-icons = {
+                notification = "<span foreground='red'><sup></sup></span>";
+                none = "";
+                dnd-notification = "<span foreground='red'><sup></sup></span>";
+                dnd-none = "";
+                inhibited-notification = "<span foreground='red'><sup></sup></span>";
+                inhibited-none = "";
+                dnd-inhibited-notification = "<span foreground='red'><sup></sup></span>";
+                dnd-inhibited-none = "";
+              };
+              return-type = "json";
+              exec-if = "which swaync-client";
+              exec = "swaync-client -swb";
+              on-click = "swaync-client -t -sw";
+              on-click-right = "swaync-client -d -sw";
+              escape = true;
+            };
+          };
+        };
+        style = "~/.dotfiles/makati-nixos/config/waybar/styles.css";
       };
     };
 
@@ -75,7 +124,7 @@ in {
     xwayland.enable = true;
 
     settings = {
-      "source" = "~/.dotfiles/makati-nixos/custom/hyprland.conf";
+      "source" = "~/.dotfiles/makati-nixos/config/hyprland/hyprland.conf";
     };
   };
 }
