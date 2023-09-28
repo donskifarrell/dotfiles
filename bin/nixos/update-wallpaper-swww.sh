d2ca95e3fd2742659cd6c40c503ed9d722a9bc9c
Donal Farrell <donskifarrell@googlemail.com>
2023-09-28 22:05:39 +0800

    swww wallpaper script

---
+++ bin/nixos/update-wallpaper-swww.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+wallpapers_path=$XDG_CACHE_HOME/wallpapers
+mkdir -p "$wallpapers_path"
+
+wallpaper=find "$wallpapers_path" -type f | shuf -n 1
+wallpaper_name=$(echo $wallpaper | sed "s|$wallpapers_path/||g")
+
+cp $wallpaper $XDG_CACHE_HOME/current_wallpaper.jpg
+
+swww img $XDG_CACHE_HOME/current_wallpaper.jpg --transition-step 20 --transition-fps=20
+
+sleep 1
+notify-send "Colors and Wallpaper updated" "with image $wallpaper_name"
+
+echo "DONE!"
