71647c619e0ec652de3ad547bb323f3a1a37bd00
Donal Farrell <donskifarrell@googlemail.com>
2025-01-13 20:51:55 +0000

    added vscode settings

--- modules/home/vscode.nix
+++ modules/home/vscode.nix
@@ -7,6 +7,7 @@ let
   vscode-marketplace = inputs.nix-vscode-extensions.extensions.${system}.vscode-marketplace;
 
   # TODO: Move to a top-level config
+  homeDir = "/${if pkgs.stdenv.isDarwin then "Users" else "home"}/df";
   system = "${if pkgs.stdenv.isDarwin then "aarch64-darwin" else "x86_64-linux"}";
 in
 {
@@ -36,5 +37,96 @@ in
       tamasfe.even-better-toml
       waderyan.gitblame
     ];
+
+    userSettings = builtins.fromJSON ''
+      {
+        "[dockerfile]": {
+          "editor.defaultFormatter": "ms-azuretools.vscode-docker"
+        },
+        "[html]": {
+          "editor.defaultFormatter": "esbenp.prettier-vscode"
+        },
+        "[json]": {
+          "editor.defaultFormatter": "vscode.json-language-features"
+        },
+        "nix.formatterPath": "nixfmt",
+        "[nix]": {
+          "editor.defaultFormatter": "jnoortheen.nix-ide"
+        },
+        "[typescript]": {
+          "editor.defaultFormatter": "esbenp.prettier-vscode"
+        },
+        "[typescriptreact]": {
+          "editor.defaultFormatter": "esbenp.prettier-vscode"
+        },
+        "go.formatTool": "gofmt",
+        "go.toolsManagement.autoUpdate": false,
+        "cSpell.language": "en-GB",
+        "diffEditor.ignoreTrimWhitespace": false,
+        "editor.bracketPairColorization.enabled": true,
+        "editor.fontFamily": "JetBrainsMono Nerd Font, 'Droid Sans Mono', 'monospace', monospace",
+        "editor.fontLigatures": true,
+        "editor.formatOnSave": true,
+        "editor.linkedEditing": true,
+        "editor.tabSize": 2,
+        "editor.wordWrap": "on",
+        "editor.unicodeHighlight.includeStrings": true,
+        "explorer.confirmDelete": false,
+        "files.trimFinalNewlines": true,
+        "files.trimTrailingWhitespace": true,
+        "files.eol": "\n",
+        "files.encoding": "utf8",
+        "files.associations": {
+          "*.tmpl": "html"
+        },
+        "git.confirmSync": false,
+        "redhat.telemetry.enabled": false,
+        "todohighlight.keywords": [
+          {
+            "text": "TODO:",
+            "color": "#fff",
+            "backgroundColor": "#ffbd2a",
+            "overviewRulerColor": "rgba(255,189,42,0.8)"
+          },
+          {
+            "text": "TODO",
+            "color": "#fff",
+            "backgroundColor": "#ffbd2a",
+            "overviewRulerColor": "rgba(255,189,42,0.8)"
+          },
+          {
+            "text": "[TODO]",
+            "color": "#fff",
+            "backgroundColor": "#ffbd2a",
+            "overviewRulerColor": "rgba(255,189,42,0.8)"
+          },
+          {
+            "text": "FIXME:",
+            "color": "#fff",
+            "backgroundColor": "#f06292",
+            "overviewRulerColor": "rgba(240,98,146,0.8)"
+          }
+        ],
+        "todohighlight.include": [
+          "**/*.js",
+          "**/*.jsx",
+          "**/*.ts",
+          "**/*.tsx",
+          "**/*.html",
+          "**/*.css",
+          "**/*.scss",
+          "**/*.php",
+          "**/*.rb",
+          "**/*.txt",
+          "**/*.mdown",
+          "**/*.md",
+          "**/*.go",
+          "**/*.tmpl",
+          "**/*.sql"
+        ],
+        "remote.SSH.configFile": "${homeDir}/.ssh/sshconfig.local",
+        "Prettier-SQL.SQLFlavourOverride": "postgresql"
+      }
+    '';
   };
 }
