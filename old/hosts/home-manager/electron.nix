9da41b87db9916c759716bfd18290814bbee2370
Donal Farrell <donskifarrell@googlemail.com>
2025-01-13 20:53:32 +0000

    cleanup

--- old/hosts/home-manager/electron.nix
+++
@@ -1,8 +0,0 @@
-{config, ...}: {
-  home = {
-    file."electron25-flags.conf" = {
-      source = "${config.home.homeDirectory}/.dotfiles/hosts/config/electron25-flags.conf";
-      target = "${config.home.homeDirectory}/.config/electron25-flags.conf";
-    };
-  };
-}
