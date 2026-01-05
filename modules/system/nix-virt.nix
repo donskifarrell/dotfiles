61a6f9f84ef5e649707b64660f1d9b667f90d100
df <donskifarrell@googlemail.com>
2026-01-05 23:05:51 +0000

    enable nixvirt

--- modules/system/nix-virt.nix
+++ modules/system/nix-virt.nix
@@ -15,57 +15,68 @@
       ];
 
       config = {
-        virtualisation.libvirt.connections."qemu:///system" = {
-          networks = [
-            {
-              definition = inputs.NixVirt.lib.network.writeXML (
-                inputs.NixVirt.lib.network.templates.nat {
-                  name = "clan-nat";
-                  subnet = "192.168.100.0/24";
-                }
-              );
-              active = true;
-            }
-          ];
+        virtualisation = {
 
-          pools = [
-            {
-              definition = inputs.NixVirt.lib.pool.writeXML (
-                inputs.NixVirt.lib.pool.templates.dir {
-                  name = "clan-vms";
-                  path = "/var/lib/libvirt/clan-vms";
-                }
-              );
-              active = true;
-            }
-          ];
+          libvirt = {
+            enable = true;
+            verbose = true;
+            swtpm.enable = true;
+          };
 
-          domains = [
-            {
-              definition = inputs.NixVirt.lib.domain.writeXML (
-                inputs.NixVirt.lib.domain.templates.nixos {
-                  name = "vm-bb";
-                  memory = 4096;
-                  vcpu = 4;
+          # Enable SPICE USB redirection if requested
+          # spiceUSBRedirection.enable = cfg.spiceUSBRedirection;
 
-                  volume = {
-                    pool = "clan-vms";
-                    size = 40 * 1024;
-                  };
+          libvirt.connections."qemu:///system" = {
+            networks = [
+              {
+                definition = inputs.NixVirt.lib.network.writeXML (
+                  inputs.NixVirt.lib.network.templates.nat {
+                    name = "clan-nat";
+                    subnet = "192.168.100.0/24";
+                  }
+                );
+                active = true;
+              }
+            ];
 
-                  network = {
-                    network = "clan-nat";
-                    model = "virtio";
-                  };
+            pools = [
+              {
+                definition = inputs.NixVirt.lib.pool.writeXML (
+                  inputs.NixVirt.lib.pool.templates.dir {
+                    name = "clan-vms";
+                    path = "/var/lib/libvirt/clan-vms";
+                  }
+                );
+                active = true;
+              }
+            ];
 
-                  nixosConfig = config.clan.machines."vm-bb".nixosConfiguration;
-                }
-              );
+            domains = [
+              {
+                definition = inputs.NixVirt.lib.domain.writeXML (
+                  inputs.NixVirt.lib.domain.templates.linux {
+                    name = "vm-bb";
+                    memory = 4096;
+                    vcpu = 4;
 
-              active = true;
-              autostart = true;
-            }
-          ];
+                    volume = {
+                      pool = "clan-vms";
+                      size = 40 * 1024;
+                    };
+
+                    network = {
+                      network = "clan-nat";
+                      model = "virtio";
+                    };
+
+                    nixosConfig = config.clan.machines."vm-bb".nixosConfiguration;
+                  }
+                );
+
+                active = true;
+              }
+            ];
+          };
         };
       };
     };
