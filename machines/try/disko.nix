610ab8c073731337515187fb062a5e7f2b25edfb
df <donskifarrell@googlemail.com>
2026-06-26 10:12:41 +0100

    machines/try: set disk schema to ext4-single-disk

---
+++ machines/try/disko.nix
@@ -0,0 +1,51 @@
+# ---
+# schema = "ext4-single-disk"
+# [placeholders]
+# mainDisk = "/dev/disk/by-path/pci-0000:04:00.0" 
+# ---
+# This file was automatically generated!
+# CHANGING this configuration requires wiping and reinstalling the machine
+{
+  boot.loader.grub = {
+    efiInstallAsRemovable = true;
+    efiSupport = true;
+  };
+
+  disko.devices = {
+    disk = {
+      main = {
+        name = "main-a3ab8217e3d24ce5882cd3bdf054096d";
+        device = "/dev/disk/by-path/pci-0000:04:00.0";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            "boot" = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+              priority = 1;
+            };
+            ESP = {
+              type = "EF00";
+              size = "500M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
